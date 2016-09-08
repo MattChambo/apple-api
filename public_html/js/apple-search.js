768272dcc9707156bbcf5694678606095a08e09a
matthew william chamberlain <845862784@kent-202-14m-2.local>
2016-09-09 11:44:07 +1200

    Finish project

--- public_html/js/apple-search.js
+++ public_html/js/apple-search.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
 		// Make suer there is something to search
 		if($.trim(searchQuery) == ''){
 
+			// You would normally put an error message here
 			// Return out of the function
 			return;
 		}
@@ -28,6 +29,70 @@ $(document).ready(function(){
 			},
 			success:function(dataFromServer){
 				console.log(dataFromServer);
+
+				// Clear old results
+				$('#results').html('');
+
+				// if there are no results
+				if(dataFromServer.resultCount == 0){
+					//Display no result message
+					$('#results').html('Sorry we could not find'+$('#apple-search select option:selected').html()+'s for '+searchQuery);
+					return;
+				}
+
+				// Loop over the results
+				for(var i = 0; i<dataFromServer.results.length; i++){
+
+					// Put row into a variable
+					var product = dataFromServer.results[i];
+
+					// Prepare a variable to hold all html
+					var div = $('<div class="search-result">');
+
+					// Inject the heading and icon into the result div
+					$(div).append('<h2><a target="blank" href="'+product.artistViewUrl+'">'+product.artistName+'</a></h2>');
+					$(div).append('<img src="'+product.artworkUrl100+'"alt="">');
+					$(div).append('<p>$'+(product.collectionPrice || product.price)+' '+product.currency+'</p>');
+
+
+					// If this product has a description
+					if(product.shortDescription){
+						$(div).append('<p>'+product.shortDescription+'...</p>');
+					}
+
+					// If this product has an audio preview
+					switch( product.kind ){
+						//Audio
+						case 'song':
+						case 'audiobook':
+						case 'mix':
+						// Append an audio element to play the preview
+						$(div).append('<audio preload="none" controls src="'+product.previewUrl+'">');
+						break;
+
+						//Video
+						case 'musicVideo':
+						case 'tv-episode':
+							$(div).append('<video preload="none" controls src="'+product.previewUrl+'">');
+					}
+					// Close the div
+					$('#results').append(div);
+				}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 			},
 			error:function(){
 
